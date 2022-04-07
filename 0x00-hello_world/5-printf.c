0f3b1158b6ae0777d159a3d0f134f4164c3266aa
BlenSeleshi <bluseleshi@gamil.com>
2022-04-07 12:11:11 -0700

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-/* using printf */
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
