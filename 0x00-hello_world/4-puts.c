069006ea3d447da4d7255b8b2d730dafdc98df52
BlenSeleshi <bluseleshi@gamil.com>
2022-04-07 11:55:43 -0700

    using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-/*printing using puts function*/
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-	puts("\"Programming is like building multilingual puzzle");
+	puts("\"Programming is building a multilingual puzzle");
 	return (0);
 }
