de8f950e1ce73d55fe8f130b47560ce9f20c95db
BlenSeleshi <bluseleshi@gamil.com>
2022-04-08 08:57:33 -0700

    print al combination of base10 digits separated by a comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,17 @@
 */
 int main(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
-	{
+	while (i < 10)
+	{ 
 		putchar(i % 10 + '0');
+		if (i < 9)
+		{
 		putchar(',');
 		putchar(' ');
+		}
+		++i;
 	}
 	putchar('\n');
 
