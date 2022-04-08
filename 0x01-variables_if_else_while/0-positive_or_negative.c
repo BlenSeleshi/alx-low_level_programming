fd369bbc2549ad74ffec95af784ae5ed78427c29
BlenSeleshi <bluseleshi@gamil.com>
2022-04-08 06:22:05 -0700

    Distingushing whether a number is positive, negative, or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
 * main - Entry point
-* 
+*
 * Return: Always 0 (Success)
 */
 int main(void)
@@ -15,11 +15,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n == 0)
-	printf("%d is zero\n", n );
+	printf("%d is zero\n", n);
 	else if (n > 0)
 	printf("%d is positive\n", n);
 	else
 	printf("%d is negative\n", n);
 
-	return (0);	
+	return (0);
 }
