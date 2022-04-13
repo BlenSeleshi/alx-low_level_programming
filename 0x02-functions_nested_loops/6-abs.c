ceaa08c7e2f2572d58c4219097bdf43d9de115c0
BlenSeleshi <bluseleshi@gamil.com>
2022-04-13 02:14:21 -0700

    writing a function that returns the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _abs - retruns the absolute value of a number
+* @n: accepts number
+* Return: the number itself if positive or zero, else the number multiplied by -1
+*/
+int _abs(int)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+}
