b699cf6c18988b2f53d9b7b6ea5e75dc5e7b073a
BlenSeleshi <bluseleshi@gamil.com>
2022-04-13 01:26:51 -0700

    wrtiting a function that prints the alphabet in small letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - Entry block
+* @void: no argument
+* return: 0
+**/
+void print_alpahbet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
